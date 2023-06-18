35fd817daed47979c9204dddacf5bf1c6bc5eb57
robin642 <wrobidean@gmail>
2023-06-18 06:58:53 -0700

    C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 /**
- * main - Prints "and that piece of art is useful" -Dora Kopar, 2015-10-19",
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  * followed by a newline, to standard error.
  * Return: Always 1.
  */
@@ -9,6 +9,6 @@
 int main(void)
 
 {
-	write(2, "and that piece of art is useful\" -Dora Kopar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

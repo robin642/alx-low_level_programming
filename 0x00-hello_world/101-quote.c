42f55c24546e898182a510376748b90c2f070318
robin642 <wrobidean@gmail>
2023-06-18 06:53:09 -0700

    C - Hello, World C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
-/**main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+/**
+ * main - Prints "and that piece of art is useful" -Dora Kopar, 2015-10-19",
  * followed by a newline, to standard error.
  * Return: Always 1.
  */

d67dc21057098de70d2a8f341b9bc4ad785425a4
robin642 <wrobidean@gmail>
2023-06-29 12:54:20 -0700

    Write a function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,40 +2,43 @@
 
 /**
   * cap_string - Write a function that capitalizes all words of a string
-  * @str: The string capitalized
-  * Return: Pointer changed string (str)
+  * @r: The pointer to string
+  * Return: The pointer to r
   */
 
-char *cap_string(char *)
+char *cap_string(char *r)
 
 {
-		int caps = 0;
+		int caps;
 
-		while (s[caps] != '\0')
+		caps = 0;
+		while (r[caps] != '\0')
 		{
+			if (r[0] >= 97 && r[0] <= 122)
+			{
+				r[0] = r[0] - 32;
+			}
 
-			if (s[caps] >= 97 && s[caps] <= 122)
-
-				caps++;
-
-
-				if (s[caps + 1] == ' ' ||
-				s[caps + 1] == '\t' ||
-				s[caps + 1] == '\n' ||
-				s[caps + 1] == '.' ||
-				s[caps + 1] == ';' ||
-				s[caps + 1] == ',' ||
-				s[caps + 1] == '!' ||
-				s[caps + 1] == '?' ||
-				s[caps + 1] == '"' ||
-				s[caps + 1] == '(' ||
-				s[caps + 1] == ')' ||
-				s[caps + 1] == '{' ||
-				s[caps + 1] == '}' ||
-				caps == 0)
-					s[caps] -= 32;
-
+				if (r[caps] == ' ' ||
+				r[caps] == '\t' ||
+				r[caps] == '\n' ||
+				r[caps] == ',' ||
+				r[caps] == ';' ||
+				r[caps] == '.' ||
+				r[caps] == '!' ||
+				r[caps] == '?' ||
+				r[caps] == '"' ||
+				r[caps] == '(' ||
+				r[caps] == ')' ||
+				r(caps)	== '{' ||
+				r[caps] == '}')
+			{
+				if (r[caps + 1] >= 97 && r[caps + 1] <= 122)
+				{
+					r[caps + 1] = r[caps + 1] - 32;
+				}
+			}
 					caps++;
 		}
-					return (s);
+					return (r);
 }

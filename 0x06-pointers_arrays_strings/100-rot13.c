8660f6f97414c48f9e416d3787801153d915d284
robin642 <wrobidean@gmail>
2023-07-02 00:38:55 -0700

    Write a function that prints a buffer

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * rot13 - Write a function that encodes a string using rot13
+  * @rob -The string targeted
+  * Return: The encoded string (rob)
+  */
+
+char *rot13(char *)
+
+{
+		int robin1, robin2;
+
+		char Alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',					'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',					'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',						'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',						'v', 'w', 'x', 'y', 'z'};

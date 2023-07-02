fb1659edf108c6b79554e80110348426b7672b2e
robin642 <wrobidean@gmail>
2023-07-02 02:24:14 -0700

    Write a function that prints an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer;
+ * @n: integer to be printed;
+ */
+void print_number(int n)
+{
+	unsigned int r1;
+
+	if (n < 0)
+	{
+		r1 = -n;
+		_putchar('-');
+	} else
+	{
+		r1 = n;
+	}
+
+	if (r1 / 10)
+		print_number(r1 / 10);
+
+	_putchar((r1 % 10) + '0');
+}

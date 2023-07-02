ae7d2b978dad226e35bf867e85159030f4fcc762
robin642 <wrobidean@gmail>
2023-07-02 11:15:17 -0700

    Write a function that adds two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,45 +9,37 @@
  * Return: address of r or 0
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
-
 {
-
-		int y, s, t, u, v, w;
-				for (y = 0; n1[y]; y++)
-					;
-				for (s = 0; n2[s]; s++)
-					;
-				if (y > size_r || s > size_r)
-
-					return (0);
-
-					v = 0;
-
-				for (y -= 1, s -= 1, t = 0; t < size_r - 1; y--, s--, t++)
-				{
-					w = v;
-
-				if (y >= 0)
-					w += n1[y] - '0';
-				if (s >= 0)
-					w += n2[s] - '0';
-				if (y < 0 && s < 0 && w == 0)
-				{
-					break;
-				}
-
-					v = w / 10;
-					r[t] = w % 10 + '0';
-				}
-					r[t] = '\0';
-				if (y >= 0 || s >= 0 || v)
-					return (0);
-
-				for (t -= 1, u = 0; u < t; t--, u++)
-				{
-					v = r[t];
-					r[t] = r[u];
-					r[u] = v;
-				}
-					return (r);
+int i, j, k, l, m, n;
+for (i = 0; n1[i]; i++)
+;
+for (j = 0; n2[j]; j++)
+;
+if (i > size_r || j > size_r)
+return (0);
+m = 0;
+for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+{
+n = m;
+if (i >= 0)
+n += n1[i] - '0';
+if (j >= 0)
+n += n2[j] - '0';
+if (i < 0 && j < 0 && n == 0)
+{
+break;
+}
+m = n / 10;
+r[k] = n % 10 + '0';
+}
+r[k] = '\0';
+if (i >= 0 || j >= 0 || m)
+return (0);
+for (k -= 1, l = 0; l < k; k--, l++)
+{
+m = r[k];
+r[k] = r[l];
+r[l] = m;
+}
+return (r);
 }

dc23706c73680c209ab9072e4335b5ceea66ecee
robin642 <wrobidean@gmail>
2023-07-03 14:51:46 -0700

    Write a function that adds two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -44,40 +44,61 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		int val1 = 0, val2 = 0, temp_tot = 0;
 
 		while (*(n1 + d) != '\0')
+		{
 			d++;
+		}
 		while (*(n2 + w) != '\0')
+		{
 			w++;
-		d--;
-		w--;
-
+			d--;
+			w--;
+		}
 		if (w >= size_r || d >= size_r)
-			return (0);
+		
+		return (0);
+		
 		while (w >= 0 || d >= 0 || overflow == 1)
 		{
 			if (d < 0)
 				val1 = 0;
 			else
+			{
 				val1 = *(n1 + d) - '0';
+			}
 			if (w < 0)
+			{
 				val2 = 0;
+			}
 			else
+			{
 				val2 = *(n2 + w) - '0';
-			temp_tot = val1 + val2 + overflow;
+				temp_tot = val1 + val2 + overflow;
+			}
 			if (temp_tot >= 10)
+			{
 				overflow = 1;
+			}
 			else
+			{
 				overflow = 0;
+			}
 			if (digits >= (size_r - 1))
-				return (0);
-			*(r + digits) = (temp_tot % 10) + '0';
-			digits++;
-			w++;
-			d++;
+				
+			return (0);
+			{
+				*(r + digits) = (temp_tot % 10) + '0';
+				digits++;
+				w++;
+				d++;
+			}
 		}
 
 		if (digits == size_r)
-			return (0);
-		*(r + digits) = '\0';
-		rev_string(r);
+			
+		return (0);
+		{
+			*(r + digits) = '\0';
+			rev_string(r);
+		}
 		return (r);
 }

96fd659ae262940b2dfdee7d788391ea7bfa04d8
robin642 <wrobidean@gmail>
2023-07-07 02:47:10 -0700

    Write a program that prints its name, followed by a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
   * main - Write a program that prints its name
   * @argc: The number of arguments
   * @argv: The array of arguments
-  * REturn: Always 0 (Success)
+  * Return: Always 0 (Success)
   */
 
 int main(int argc __attribute__((unused)), char *argv[])

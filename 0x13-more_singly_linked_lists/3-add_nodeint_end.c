6d43fd02be0efe69eee5f025a2a8be66d66016ca
robin642 <wrobidean@gmail>
2023-07-31 13:26:01 -0700

    Write a function that adds a new node at the end of a listint_t list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,7 +14,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 {
 		listint_t *swap_node = (listint_t *)malloc(sizeof(listint_t));
-		
+
 		if (swap_node == NULL)
 		{
 			return (NULL);

3e7e0a743c8ddcf297f737626ee8ce5efb566e11
robin642 <wrobidean@gmail>
2023-08-02 11:37:24 -0700

    Write a function that frees a listint_t list

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint_safe - It frees a listint_t list
+ * @h: The pointer to first node
+ * Return: The number of elements in the freed list
+ */
+
+size_t free_listint_safe(listint_t **h)
+
+{
+		size_t expect = 0;
+		int make;
+		listint_t *change;
+
+		if (!h || !*h)
+			return (0);
+	
+		while (*h)
+		{
+			make = *h - (*h)->next;
+			if (make > 0)
+			{
+				change = (*h)->next;
+				free(*h);
+				*h = change;
+				expect++;
+			}
+			else
+			{
+				free(*h);
+				*h = NULL;
+				expect++;
+				break;
+			}
+		}
+
+		*h = NULL;
+
+		return (expect);
+}

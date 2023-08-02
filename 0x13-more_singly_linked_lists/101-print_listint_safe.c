b853c64450ca0e80007c34450db5b53c91b536cb
robin642 <wrobidean@gmail>
2023-08-02 11:51:56 -0700

    Write a function that prints a listint_t linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -4,6 +4,52 @@
 #include "lists.h"
 
 /**
+ * looped_listint_len - Counts the number of unique nodes
+ * @head: A pointer to the head
+ * Return: 0
+ */
+size_t looped_listint_len(const listint_t *head)
+{
+	const listint_t *lemon, *orange;
+	size_t count = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	lemon = head->next;
+	orange = (head->next)->next;
+
+	while (orange)
+	{
+		if (lemon == orange)
+		{
+			lemon = head;
+			while (lemon != orange)
+			{
+				count++;
+				lemon = lemon->next;
+				orange = orange->next;
+			}
+
+			lemon = lemon->next;
+			while (lemon != orange)
+			{
+				count++;
+				lemon = lemon->next;
+			}
+
+			return (count);
+		}
+
+		lemon = lemon->next;
+		orange = (orange->next)->next;
+	}
+
+	return (0);
+}
+
+
+/**
  * print_listint_safe - It prints a linked list safely
  * @head: The type listint_t to print
  * Return: The number of nodes

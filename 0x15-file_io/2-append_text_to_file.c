b316a2f17bb683331ccb78a49151ca5314803fae
robin642 <wrobidean@gmail>
2023-08-07 14:46:07 -0700

    Write a function that appends text at the end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -30,7 +30,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		}
 
 		p = open(filename, O_WRONLY | O_APPEND);
-		
+
 		pages = (write(p, text_content, answer) < 0);
 
 		if (p == -1 || pages == -1)

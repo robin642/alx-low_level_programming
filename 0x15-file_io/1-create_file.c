2818de85290f4d6f6a976d11e8a6f703eb44baf6
robin642 <wrobidean@gmail>
2023-08-08 08:57:51 -0700

    Create a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 				answer++;
 		}
 
-		pages = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+		pages = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 		p = write(pages, text_content, answer);
 

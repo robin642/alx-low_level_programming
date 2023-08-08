7f451749b59b4d1ff1b51ac53c612fbdd9fa2e88
robin642 <wrobidean@gmail>
2023-08-08 10:08:17 -0700

    Create a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,29 +13,31 @@
 int create_file(const char *filename, char *text_content)
 
 {
-		int pages, answer = 0;
+		int pages = 0;
+		int answer = 0;
+		int p = 0;
 
-
-		if (!filename)
+		if (filename == NULL)
 
 			return (-1);
 
-		if (!text_content)
-
-			text_content = "";
-
-			while (text_content[answer] != '\0')
-			{
+		if (text_content != NULL)
+		{
+			for (answer = 0; text_content[answer];)
+			
 				answer++;
-			}
+		}
 
 		pages = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-		write(pages, text_content, answer);
 
-		if (pages == -1)
+		if (pages == -1 || p == -1)
 
 			return (-1);
+
+		p = write(pages, text_content, answer);
+
+		close(pages);
 
 		return (1);
 }
